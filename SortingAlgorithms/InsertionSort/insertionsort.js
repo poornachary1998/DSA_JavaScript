0dab7c25a81a4c4dd3d8a8c59fb3d2abdb32e698
Poorna chary <poornachary1@gmail.com>
2025-05-12 21:58:24 +0530

    insertion sort

---
+++ SortingAlgorithms/InsertionSort/insertionsort.js
@@ -0,0 +1,14 @@
+function insertionSort(arr){
+
+    for(let i = 1; i<arr.length; i++){
+        let currentval = arr[i]
+        for( var j = i-1; j>=0 && arr[j]>currentval; j--){
+                arr[j+1] = arr[j]           
+        }
+         arr[j+1] = currentval;
+
+    }
+
+return arr;
+}
+console.log(insertionSort([1,4,2,3]))
