f5287f59b6e620cbb055cb5b683f1f5675fe710d
Poorna chary <poornachary1@gmail.com>
2025-05-17 16:25:15 +0530

    QuickSort

--- SortingAlgorithms/QuickSort/QuickSortHelper.js
+++ SortingAlgorithms/QuickSort/QuickSortHelper.js
@@ -1,7 +1,7 @@
 function QuickSortHelper(arr, start = 0 , end = arr.length-1){
     let pivotValue = arr[start];
     let pivotIndex = start;
-for(let i = start +1;i< end;i++){
+for(let i = start +1;i<=end;i++){
     if(arr[i]< pivotValue){
         pivotIndex++;
         [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]]
@@ -10,4 +10,15 @@ for(let i = start +1;i< end;i++){
 [arr[start], arr[pivotIndex]] = [arr[pivotIndex], arr[start]]
 return pivotIndex
 }
-console.log(QuickSortHelper([5,2,1,8,4,7,6,3]))
+
+function QuickSort(arr, left=0, right = arr.length-1 ){
+if(left<right){
+let pivotIndex = QuickSortHelper(arr, left, right)
+
+//
+QuickSort(arr, left, pivotIndex-1)
+QuickSort(arr, pivotIndex+1, right)
+}
+return arr
+}
+console.log(QuickSort([12,3,1,5,8,2]))
