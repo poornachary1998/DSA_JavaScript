c7a92c27c38a04ca2125f06a46ef075378d97ef0
Poorna chary <poornachary1@gmail.com>
2025-05-19 12:22:18 +0530

    DSA intro Instance methods

--- DataStructures/01_Introduction.js
+++ DataStructures/01_Introduction.js
@@ -19,3 +19,23 @@ let secondStudent = new Student("Charan", "N");
 secondStudent.education = 10
 console.log(secondStudent);
 // Student { firstName: 'Charan', lastName: 'N', education: 10 }
+
+
+//Instance Methods.
+
+
+class HighSchoolStudent{
+    constructor(firstName, lastName){
+this.firstName = firstName;
+this.lastName =lastName;
+    }
+    fullName(){
+        return `your fullName is ${this.firstName} ${this.lastName}`
+    }
+}
+let student1 = new HighSchoolStudent("james", "son")
+console.log(student1)
+// HighSchoolStudent { firstName: 'james', lastName: 'son' }
+
+console.log(student1.fullName())
+// your fullName is james son
