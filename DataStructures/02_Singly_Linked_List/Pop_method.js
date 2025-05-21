1f874a757e8fbe932e3a069624d74d1d0ba19ff7
Poorna chary <poornachary1@gmail.com>
2025-05-21 12:02:06 +0530

    Added Pop method logic

--- DataStructures/02_Singly_Linked_List/Pop_method.js
+++ DataStructures/02_Singly_Linked_List/Pop_method.js
@@ -31,14 +31,33 @@ class SinglyLinkedList{
         return this;
     }
     //pop method
-    traverse(){
-        
+    pop(){
+        if(!this.head) return undefined;
+        let current = this.head;
+        let newtail = current;
+        while(current.next){
+            newtail = current;
+            current  = current.next;
+        }
+        this.tail = newtail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length ===0){
+            this.head = null;
+            this.tail = null;
+        }
+        return current, this;
     }
 }
 
-let SLL = new SinglyLinkedList();
-SLL.push("Hey");
-SLL.push("I dont know");
-SLL.push("!")
-
-console.log(SLL)
+let list = new SinglyLinkedList();
+list.push("Hey");
+list.push("I dont know");
+list.push("!")
+console.log(list)
+list.pop()
+console.log(list)
+list.pop()
+console.log(list)
+list.pop()
+console.log(list)
