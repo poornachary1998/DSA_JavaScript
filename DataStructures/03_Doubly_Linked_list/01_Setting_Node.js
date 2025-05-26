c33f36f2e38f8965eff6321127d4d0dae018ea84
Poorna chary <poornachary1@gmail.com>
2025-05-26 20:04:26 +0530

    Double Linked List - Introduction

---
+++ DataStructures/03_Doubly_Linked_list/01_Setting_Node.js
@@ -0,0 +1,20 @@
+// Double linked list, each node will have 2 directions,
+//next
+//prev
+
+//setting up the Node class.
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}
