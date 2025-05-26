7b4ba1c9cdbc45b354e9091bc6dabde1f3f749af
Poorna chary <poornachary1@gmail.com>
2025-05-26 22:14:12 +0530

    get method

---
+++ DataStructures/03_Doubly_Linked_list/06_get_method.js
@@ -0,0 +1,139 @@
+//shift method: Doubly Linked list
+//psuedo code:
+
+// if there is no tail and no head retrun undefined.
+// Store the current tail in a variable to retrurn later.
+// If the length is 1, set the head and tail property to be null.
+// update the head to be next of the old head.
+// set the heads prev property to null.
+// set the old head next to null.
+// Decrement the length;
+// return value removed / this
+
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.prev = null;
+        this.next = null;
+    }
+}
+class DoublyLinkedList {
+    constructor() {
+        this.length = 0;
+        this.next = null;
+        this.prev = null;
+    }
+    push(val) {
+        let newNode = new Node(val);
+        //  let current = this.head
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        }
+        else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail
+
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if (!this.head) return undefined;
+        let tail = this.tail;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = this.tail.prev
+            tail.prev = null;
+            this.tail.next = null;
+        }
+        this.length--;
+        return console.log(tail);
+    }
+    shift() {
+        if (!this.head) return undefined;
+        let currentHead = this.head;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = currentHead.next;
+            this.head.prev = null;
+            currentHead.next = null;
+        }
+        this.length--;
+        return this;
+    }
+    unshift(val) {
+        let newNode = new Node(val);
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            // newNode.prev = null;
+            this.head = newNode
+        }
+        this.length++
+        return this;
+    }
+
+    get(idx) {
+        if (idx < 0 || idx >= this.length) return null;
+        let middle = Math.floor(this.length / 2);
+        let currenthead = this.head;
+        let currentTail = this.tail
+        let counterHead = 0;
+        let counterTail = this.length - 1
+        if (idx <= middle) {
+            console.log("starting")
+            while (idx !== counterHead) {
+                currenthead = currenthead.next;
+                counterHead++;
+            }
+            return currenthead
+        } else {
+            console.log("ending")
+            while (idx !== counterTail) {
+                currentTail = currentTail.prev
+                counterTail--
+            }
+            return currentTail
+        }
+    }
+    printDLL() {
+        let current = this.head;
+        while (current) {
+            console.log(
+                `Val: ${current.val}, Prev: ${current.prev?.val ?? 'null'}, Next: ${current.next?.val ?? 'null'}`
+            );
+            current = current.next;
+        }
+    }
+}
+
+let DLL = new DoublyLinkedList();
+DLL.push(10);
+DLL.push(20);
+DLL.push(30);
+DLL.push(40);
+DLL.push(50);
+DLL.push(60);
+// DLL.push(70);
+
+DLL.printDLL()
+console.log("------")
+let foundOne = DLL.get(3)
+console.log(foundOne)
+// Val: 10, Prev: null, Next: 20
+// Val: 20, Prev: 10, Next: 30
+// Val: 30, Prev: 20, Next: null
+// ------
+// Val: 20, Prev: null, Next: 30
+// Val: 30, Prev: 20, Next: null
