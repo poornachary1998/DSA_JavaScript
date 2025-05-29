8397f095dbf779ee6d3beabf8042b29ad26eeb98
Poorna chary <poornachary1@gmail.com>
2025-05-29 11:21:09 +0530

    Push method - stack

--- DataStructures/04_Stacks_Queues/01_Stack_LinkedList.js
+++ DataStructures/04_Stacks_Queues/01_Stack_LinkedList.js
@@ -13,4 +13,36 @@ class Stack{
         this.last = null;
         this.size = 0
     }
+    push(val){
+        let newNode = new Node(val);
+
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else{
+        let temp = this.first;
+         this.first = newNode;
+        newNode.next = temp;
+      
+
+        }
+        this.size++
+        return this
+    }
+    print(){
+        let current = this.first;
+        while (current) {
+            console.log(
+                `Val: ${current.val}, Next: ${current.next?.val ?? 'null'}`
+            );
+            current = current.next;
+        }
+    }
 }
+
+let stackSLL = new Stack;
+stackSLL.push(10)
+stackSLL.push(20)
+stackSLL.push(30)
+stackSLL.push(40)
+stackSLL.print()
