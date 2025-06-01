ab6073a7ba68b8415f7c614f2d8edfc3082aeea4
Poorna chary <poornachary1@gmail.com>
2025-06-01 16:49:03 +0530

    BST- Insert method

--- DataStructures/05_Binary_Search_Trees/01_BST.js
+++ DataStructures/05_Binary_Search_Trees/01_BST.js
@@ -10,12 +10,49 @@ class BinarySearchTree{
     constructor(){
         this.root = null;
     }
+    insert(val){
+    let newNode = new Node(val);
+    
+    if(!this.root){
+        this.root = newNode;
+        return this;
+    } else {
+       let current = this.root;
+       while(true){
+            if(val === current.value) return undefined;
+                if(val < current.value){
+                    if(current.left === null){
+                        current.left = newNode;
+                        return this;
+                         } else { 
+                            current = current.left
+                                }} else if(val >  current.value){
+                                        if(current.right === null){
+                                            current.right = newNode;
+                                            return this;
+                                        } else {
+                                            current = current.right
+                                        }
+                                }
+                }
+    }
+    }
+   
+
 }
 
 let BST = new BinarySearchTree();
 BST.root = new Node(10);
-BST.root.left = new Node(5);
-BST.root.right = new Node(15);
-BST.root.left.left = new Node(2);
-BST.root.left.right = new  Node(7);
-console.log(BST)
+BST.insert(5);
+BST.insert(20)
+BST.insert(12)
+BST.insert(22)
+BST.insert(1)
+BST.insert(7)
+BST.insert(11)
+BST.insert(17)
+console.dir(BST.root, { depth: null });
+// BST.printLevelOrder()
+    //         10
+    //     5          20
+    // 1       7   11      21
