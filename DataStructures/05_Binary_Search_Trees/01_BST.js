94e279f2dab29c66d0c037533985524c432b41d4
Poorna chary <poornachary1@gmail.com>
2025-06-02 23:25:11 +0530

    Find Method in BST

--- DataStructures/05_Binary_Search_Trees/01_BST.js
+++ DataStructures/05_Binary_Search_Trees/01_BST.js
@@ -10,34 +10,106 @@ class BinarySearchTree{
     constructor(){
         this.root = null;
     }
-    insert(val){
-    let newNode = new Node(val);
+    // insert(val){
+    // let newNode = new Node(val);
     
-    if(!this.root){
+    // if(!this.root){
+    //     this.root = newNode;
+    //     return this;
+    // } else {
+    //    let current = this.root;
+    //    while(true){
+    //         if(val === current.value) return undefined;
+    //             if(val < current.value){
+    //                 if(current.left === null){
+    //                     current.left = newNode;
+    //                     return this;
+    //                      } else { 
+    //                         current = current.left
+    //                             }} else if(val >  current.value){
+    //                                     if(current.right === null){
+    //                                         current.right = newNode;
+    //                                         return this;
+    //                                     } else {
+    //                                         current = current.right
+    //                                     }
+    //                             }
+    //             }
+    // }
+    // }
+   insert(val){
+    let newNode = new Node(val);
+    if(this.root === null){
         this.root = newNode;
         return this;
     } else {
-       let current = this.root;
-       while(true){
-            if(val === current.value) return undefined;
-                if(val < current.value){
-                    if(current.left === null){
-                        current.left = newNode;
-                        return this;
-                         } else { 
-                            current = current.left
-                                }} else if(val >  current.value){
-                                        if(current.right === null){
-                                            current.right = newNode;
-                                            return this;
-                                        } else {
-                                            current = current.right
-                                        }
-                                }
+        let current = this.root;
+        while(true){
+                    if( val === current) return undefined;
+
+            if(val < current.value){
+                if(current.left === null){
+                    current.left = newNode;
+                    return this
+                } else {
+                    current = current.left;
                 }
+
+            } else if( val > current.value){
+                if(current.right === null){
+                    current.right = newNode;
+                    return this;
+                }else {
+                    current = current.right;
+                }
+            }
+        }
     }
+
+   }
+
+//    find(val){
+//     if(!this.root) return undefined;
+//     if(val === this.root){
+//         return val;
+//     } else {
+//         let foundNode = false
+//         var currentNode = this.root;
+//         while(true && !foundNode)
+//         if(val < currentNode.value ){
+//             if(currentNode.left === val){
+//                 foundNode = true;
+//                 return currentNode.left
+//             } else {
+//                 currentNode =currentNode.left
+//             }
+//         } else if(val> currentNode.value){
+//             if(currentNode.right === val){
+//                 foundNode= true;
+//                 return currentNode.right;
+//             } else {
+//                 currentNode = currentNode.right
+//             }
+//         }
+//     }
+//    }
+find(val) {
+  if (!this.root) return undefined;
+
+  let currentNode = this.root;
+
+  while (currentNode) {
+    if (val === currentNode.value) {
+      return console.log(currentNode);
+    } else if (val < currentNode.value) {
+      currentNode = currentNode.left;
+    } else {
+      currentNode = currentNode.right;
     }
-   
+  }
+
+  return undefined; // Not found
+}
 
 }
 
@@ -51,6 +123,7 @@ BST.insert(1)
 BST.insert(7)
 BST.insert(11)
 BST.insert(17)
+BST.find(11)
 console.dir(BST.root, { depth: null });
 // BST.printLevelOrder()
     //         10
