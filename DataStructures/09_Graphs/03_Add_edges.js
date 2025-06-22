7288405fb0aa5918884c3d34fcdef91f06df9c40
Poorna chary <poornachary1@gmail.com>
2025-06-22 13:22:03 +0530

    Add Edge

---
+++ DataStructures/09_Graphs/03_Add_edges.js
@@ -0,0 +1,33 @@
+//Declaring graph class;
+
+class Graph {
+    constructor(vertex) {
+        this.adjacentList = {}
+    }
+    addVertex(vertex){
+        if(!this.adjacentList[vertex])
+        this.adjacentList[vertex] = [];
+    }
+    addEdge(v1,v2){
+        this.adjacentList[v1].push(v2);
+        this.adjacentList[v2].push(v1);
+    }
+}
+
+let g = new Graph();
+// g.addVertex("Tokyo");
+// g.addVertex("sanfranscio");
+// g.adjacentList["sanfranscio"].push("Something")
+g.addVertex("Dallas");
+g.addVertex("Boston");
+g.addVertex("Neworleans")
+g.addEdge("Dallas", "Neworleans")
+g.addEdge("Boston","Neworleans")
+console.log(g)
+// Graph {
+//   adjacentList: {
+//     Dallas: [ 'Neworleans' ],
+//     Boston: [ 'Neworleans' ],
+//     Neworleans: [ 'Dallas', 'Boston' ]
+//   }
+// }
