8a78bb40fc73a28c91873d14670158e8172c09bf
Poorna chary <poornachary1@gmail.com>
2025-06-24 12:31:57 +0530

    DFS_Recursive

---
+++ DataStructures/10_Graph_traversal/DFS_Recursive.js
@@ -0,0 +1,66 @@
+class Graph{
+    constructor(){
+        this.adjacencyList = {};
+    }
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+    addEdge(v1,v2){
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    }
+    removeEdge(vertex1,vertex2){
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+            v => v !== vertex2
+        );
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+            v => v !== vertex1
+        );
+    }
+    removeVertex(vertex){
+        while(this.adjacencyList[vertex].length){
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+        delete this.adjacencyList[vertex]
+    }
+    depthFirstSearch_Recursive(start){
+        const result = [];
+        const visitedVertex = {};
+        const adjacencyList = this.adjacencyList;
+
+        (function dfs(vertex){
+            if(!vertex){
+                return null;
+            }
+            visitedVertex[vertex] = true;
+            result.push(vertex);
+
+            adjacencyList[vertex].forEach(neighbor => {
+                if(!visitedVertex[neighbor]){
+                 return   dfs(neighbor);
+                }
+            });
+        })(start);
+        return result;
+    }
+}
+
+let g = new Graph();
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E")
+g.addVertex("F")
+
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+
+g.depthFirstSearch_Recursive("A")
+console.log(g.depthFirstSearch_Recursive("A"))
